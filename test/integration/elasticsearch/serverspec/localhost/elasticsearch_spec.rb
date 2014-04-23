49e7c24da586e514f41a1a5064089b13af971cc1
James Smith <james@floppy.org.uk>
2014-04-23 10:39:30 +0100

    Check elasticsearch configuration

--- test/integration/elasticsearch/serverspec/localhost/elasticsearch_spec.rb
+++ test/integration/elasticsearch/serverspec/localhost/elasticsearch_spec.rb
@@ -5,10 +5,19 @@ describe command("ruby -v") do
   it { should return_stdout /1.9.3/ }
 end
 
+# Make sure elasticsearch is installed
 describe file("/usr/local/bin/elasticsearch") do
   it { should be_file }
 end
 
+# Make sure elasticsearch is running
 describe service("elasticsearch") do
   it { should be_running }
+end
+
+# Make sure elasticsearch is configured correctly
+describe command("service elasticsearch status -v") do
+  it { should return_stdout /elasticsearch 0.90.12 running/ }
+  it { should return_stdout /OpenJDK 64-Bit Server VM/ }
+  it { should return_stdout /238mb/ }
 end
