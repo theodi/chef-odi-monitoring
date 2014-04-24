8e3a8355fa3d16aee04ba6b3f8c89f85c53f48b9
James Smith <james@floppy.org.uk>
2014-04-24 16:50:02 +0100

    Check that serverdensity agent is configured

--- test/integration/elasticsearch/serverspec/localhost/monitoring_spec.rb
+++ test/integration/elasticsearch/serverspec/localhost/monitoring_spec.rb
@@ -4,3 +4,9 @@ require 'spec_helper'
 describe service("sd-agent") do
   it { should be_running }
 end
+
+# Make sure serverdensity agent has got a key
+describe file("/etc/sd-agent/config.cfg") do
+  its(:content) { should match /sd_url: https:\/\/theodi.serverdensity.io/ }
+  its(:content) { should match /agent_key: [0-9a-f]{32}/ }
+end
